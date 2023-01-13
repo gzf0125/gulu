1916a8cdd1265ce471bac5f1e2db1068741ff25e
7% <gzf0125@qq.com>
2023-01-13 16:38:33 +0800

    实现购物车菜品添加减少功能

--- src/main/java/com/gulu/Controller/ShoppingCartController.java
+++ src/main/java/com/gulu/Controller/ShoppingCartController.java
@@ -29,30 +29,65 @@ public class ShoppingCartController {
         return R.success(shoppingCarts);
     }
 
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     * @return
+     */
     @PostMapping("/add")
     public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
         Long currentId = BaseContext.getCurrentId();
+        //设置当前用户id
         shoppingCart.setUserId(currentId);
         LambdaQueryWrapper<ShoppingCart> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(shoppingCart.getDishId()!=null,ShoppingCart::getDishId,shoppingCart.getDishId());
         queryWrapper.eq(shoppingCart.getSetmealId()!=null,ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
-        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
-        if (list!=null){
-            if (shoppingCart.getDishId()!=null){
+        queryWrapper.eq(shoppingCart.getDishFlavor()!=null,ShoppingCart::getDishFlavor,shoppingCart.getDishFlavor());
+        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        ShoppingCart one = shoppingCartService.getOne(queryWrapper);
+        //查询当前菜品或套餐是否已经在购物车,如果口味不同新建一条信息
+        if (one!=null){
+
+            if (shoppingCart.getDishId()!=null||shoppingCart.getSetmealId()!=null){
+                one.setNumber(one.getNumber()+1);
             }
             LambdaUpdateWrapper<ShoppingCart> updateWrapper=new LambdaUpdateWrapper<>();
             updateWrapper.eq(shoppingCart.getDishId()!=null,ShoppingCart::getDishId,shoppingCart.getDishId());
             updateWrapper.eq(shoppingCart.getSetmealId()!=null,ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
-            boolean update = shoppingCartService.update(updateWrapper);
+            updateWrapper.eq(ShoppingCart::getUserId,currentId);
+            boolean update = shoppingCartService.update(one,updateWrapper);
         }else shoppingCartService.save(shoppingCart);
         return R.success(shoppingCart);
     }
 
     @PostMapping("/sub")
-    public R<ShoppingCart> sub(@RequestBody Dish dish){
-        log.info(dish.toString());
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart){
+        Long currentId = BaseContext.getCurrentId();
+        //设置当前用户id
+        shoppingCart.setUserId(currentId);
+        LambdaQueryWrapper<ShoppingCart> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(shoppingCart.getDishId()!=null,ShoppingCart::getDishId,shoppingCart.getDishId());
+        queryWrapper.eq(shoppingCart.getSetmealId()!=null,ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        queryWrapper.eq(shoppingCart.getDishFlavor()!=null,ShoppingCart::getDishFlavor,shoppingCart.getDishFlavor());
+        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        ShoppingCart one = shoppingCartService.getOne(queryWrapper);
+        //查询当前菜品或套餐是否已经在购物车,如果口味不同新建一条信息
+        if (one!=null){
+            if (shoppingCart.getDishId()!=null||shoppingCart.getSetmealId()!=null){
+                one.setNumber(one.getNumber()-1);
+                if (one.getNumber()==0){
+                    shoppingCartService.remove(queryWrapper);
+                }
+            }
+            LambdaUpdateWrapper<ShoppingCart> updateWrapper=new LambdaUpdateWrapper<>();
+            updateWrapper.eq(shoppingCart.getDishId()!=null,ShoppingCart::getDishId,shoppingCart.getDishId());
+            updateWrapper.eq(shoppingCart.getSetmealId()!=null,ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+            updateWrapper.eq(shoppingCart.getDishFlavor()!=null,ShoppingCart::getDishFlavor,shoppingCart.getDishFlavor());
+            updateWrapper.eq(ShoppingCart::getUserId,currentId);
+            shoppingCartService.update(one,updateWrapper);
+        }
+        return R.success(shoppingCart);
 
-        return null;
     }
 
     @DeleteMapping("/clean")
