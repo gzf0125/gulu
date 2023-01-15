7c80a78175d62a23509a70364332f5100d7136a3
7% <gzf0125@qq.com>
2023-01-15 14:07:03 +0800

    构建Redis环境

--- src/test/java/test/UploadFileTest.java
+++ src/test/java/test/UploadFileTest.java
@@ -1,6 +1,9 @@
 package test;
 
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
 
 public class UploadFileTest {
     @Test
@@ -8,5 +11,19 @@ public class UploadFileTest {
         String fileName="safsa.jpg";
         String suffix=fileName.substring(fileName.lastIndexOf("."));
         System.out.println(suffix);
+    }
+    @Test
+    public void testRedis(){
+        //1、获取连接
+        Jedis jedis=new Jedis("localhost",6379);
+        //2、执行具体的操作
+        jedis.set("username","xiaoming");
+        System.out.println(jedis.get("username"));
+        jedis.hset("hash", "name1", "xiaohua");
+        System.out.println(jedis.hget("hash","name1"));
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
+        //3、关闭连接
+        jedis.close();
     }
 }
